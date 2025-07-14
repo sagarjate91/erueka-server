84011c5bf9e20b60cbeeb2342e3246e59049116b
sagarjate91 <jate.sagar91@gmail.com>
2025-07-14 15:12:29 +0530

    Initial commit

---
+++ src/test/java/com/erueka/erueka_server/EruekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erueka.erueka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EruekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
